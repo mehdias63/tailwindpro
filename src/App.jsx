cc20c8e9524df91002b8e813566fdd11a209a9fa
mehdias63 <137681653+mehdias63@users.noreply.github.com>
2023-10-01 11:06:49 +0330

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,23 @@ function App() {
 
   return (
     <>
-        <div className='bg-white'>
-          <h1 className='bg-gray-300'>The title of the card here</h1>
-          <div className='flex'>
+        <div className='bg-white w-2/5 mx-auto my-32 rounded-md overflow-hidden'>
+          <h1 className='bg-gray-200 text-gray-700 px-6 py-4'>The title of the card here</h1>
+          <div className='flex justify-between items-center px-6 py-4'>
           <button className='m-4 rounded-full text-white px-4 py-2  text-sm bg-orange-600'>UNDER REVIEW</button>
-          <p>May14,1988</p>
+          <p className='text-sm'>May14,1988</p>
           </div>
-          <div>
-            <h1 className='bg-gray-300 m-4'>here is a short comment about this emploee</h1>
+          <div className='border-t border-gray-200'>
+            <h1 className='bg-gray-200 m-4 p-10 text-xl font-medium rounded-md'>Here is a short comment about this<br/> emploee</h1>
           </div>
-          <div className='bg-gray-300'>
-            <h3>EMPLOEE</h3>
+          <div className='bg-gray-200 p-4'>
+            <h3 className='text-gray-600 mb-4 ml-3'>EMPLOEE</h3>
+            
             <div className='flex'>
-              <div className='w-16 h-16 bg-blue-600 rounded-full text-center p-4 text-white font-bold'>VG</div>
-              <div className='ml-4'>
-                <h1>Victor Gonzales</h1>
-                <p>instructor</p>
+              <div className='w-16 h-16 bg-blue-700 rounded-full text-center p-4 text-white font-bold'>VG</div>
+              <div className='ml-5'>
+                <h1 className='font-bold'>Victor Gonzales</h1>
+                <p className='text-gray-600'>instructor</p>
               </div>
             </div>
           </div>
